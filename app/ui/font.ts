0767b0f39908d3d47eb85fe2fc29ccf76b431e6d
emil0519 <r09343018@ntu.edu.tw>
2023-12-12 23:37:08 +0800

    core: Restructure files

---
+++ app/ui/font.ts
@@ -0,0 +1,5 @@
+import { Raleway, Lusitana } from 'next/font/google';
+
+export const raleWay = Raleway({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
